682a42bde1cbf544eb679c3f276b99ca309dab4c
RainyCl0ud <galario.rainheart16@gmail.com>
2025-03-05 04:36:18 +0800

    updated entrypoint and nginx files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Set correct permissions
+# Set correct permissions for storage and cache
 chmod -R 777 /app/storage /app/bootstrap/cache
 
-# Start PHP-FPM
+# Start PHP-FPM in the background
 php-fpm -D
 
-# Start Nginx
+# Start Nginx in the foreground (so Railway doesn't stop it)
 nginx -g "daemon off;"
