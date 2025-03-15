e9736025c26d701bcfadfae3a4a46570624d3a74
RainyCl0ud <galario.rainheart16@gmail.com>
2025-03-15 21:33:46 +0800

    added Procfile

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://tajms-production-93d1.up.railway.app'),
 
     /*
     |--------------------------------------------------------------------------
