5e404d2c15e83e0f3143ccccb48de84bc5c96834
RainyCl0ud <128013461+RainyCl0ud@users.noreply.github.com>
2025-05-15 13:17:15 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use App\Services\GoogleDriveService; // Make sure to import the service
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,20 +13,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Register GoogleDriveService as a singleton
+        $this->app->singleton(GoogleDriveService::class, function ($app) {
+            return new GoogleDriveService();
+        });
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot()
+    public function boot(): void
     {
-        if(config('app.env') === 'production') {
-            \URL::forceScheme('https');
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
         }
-        
+
         view()->composer('components.header', function ($view) {
             $view->with('pageTitle', $view->getData()['pageTitle'] ?? 'Dashboard');
         });
     }
-}    
+}
